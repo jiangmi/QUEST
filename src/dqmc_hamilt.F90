3c56e7e274bb59de63d907e044e8679901fd48be
mijiang <mijiang@head.cluster>
2019-10-28 09:29:47 -0700

    initial try in hamilt.F90, need check

--- src/dqmc_hamilt.F90
+++ src/dqmc_hamilt.F90
@@ -547,8 +547,13 @@ contains
           localtmp(jclass) = iclass
           !Save value of mu and U for this class
           Utmp(jclass) = hamilt%Uv(isite,isite)
-          muup(jclass) = -dble(hamilt%hopup(isite,isite))+hamilt%mu_up
-          mudn(jclass) = -dble(hamilt%hopdn(isite,isite))+hamilt%mu_dn
+          if (mod(isite,2)==0) then
+            muup(jclass) = -dble(hamilt%hopup(isite,isite))+hamilt%mu_up
+            mudn(jclass) = -dble(hamilt%hopdn(isite,isite))+hamilt%mu_dn
+          else
+            muup(jclass) = -dble(hamilt%hopup(isite,isite))
+            mudn(jclass) = -dble(hamilt%hopdn(isite,isite))
+          endif
        endif
     
        !assign site to a class
